20ab5adde3251203839cab258796dfd98b8e9414
William <ZhengWilliam@knights.ucf.edu>
2021-06-02 18:31:52 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//Be sure the output contains the original string.
+// Use a single output statement to construct the output.
+// Use a built-in function of the programming language to determine the length of the string.
+public class App {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        App myApp = new App();
+        String str = myApp.readString();
+        System.out.println(str + " has " + str.length() + " characters.");
+    }
+
+    // Reads User String
+    public String readString() {
+        System.out.print("What is the input string? ");
+        return input.nextLine();
+    }
+}
